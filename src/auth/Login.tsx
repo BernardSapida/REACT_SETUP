ae02109d5196972672e92a2eec67250f1a9a946e
BernardSapida <BernardSapida1706@gmail.com>
2023-02-26 15:31:47 +0800

    first commit

---
+++ src/auth/Login.tsx
@@ -0,0 +1,13 @@
+import Button from "react-bootstrap/Button";
+
+function Login() {
+  return (
+    <>
+      <Button as="a" variant="primary">
+        Button as link
+      </Button>
+    </>
+  );
+}
+
+export default Login;
